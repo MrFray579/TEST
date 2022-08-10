b0e778074c8878db565620a95ed15a10aaf341f3
Fray <kozar20000@mail.ru>
2022-08-10 15:28:04 +0300

    programs

---
+++ aTask/Program.cs
@@ -0,0 +1,32 @@
+﻿string [] arrayString = {"job", "work", "git", "help", "2"};
+int len = arrayString.Length;
+int count1 = 0;
+
+
+
+for (int i = 0; i < len; i++)
+{
+    string a = arrayString[i];
+    if (a.Length <= 3)
+    {
+        count1++;
+    } 
+}
+
+string [] arrNew = new string[count1];
+int count2 = 0;
+
+for (int i = 0; i < len; i++)
+{
+    string a = arrayString[i];
+    if (a.Length <= 3)
+    {
+        for (int j = 0; j < count1; j++)
+        {
+            arrNew[count2] = a;
+        }
+        count2++;
+    }
+}
+
+Console.WriteLine("[" + string.Join(", ", arrayString) + "]" + " - " + "[" + string.Join(", ", arrNew) + "]");
